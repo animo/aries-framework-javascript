96f00e1308b55576709fb8d2e7c8d7503460877c
Berend Sliedrecht <blu3beri@proton.me>
2023-07-09 14:03:29 +0200

    feat: sd-jwt using jwt-sd library
    Signed-off-by: Berend Sliedrecht <blu3beri@proton.me>

--- packages/sd-jwt/src/SdJwtApi.ts
+++ packages/sd-jwt/src/SdJwtApi.ts
@@ -1,9 +1,17 @@
-import type { VERIFYSDJWT_RETURN } from './services'
 import type { Key } from '@aries-framework/core'
 
-import { AgentContext, ConnectionService, injectable, MessageSender } from '@aries-framework/core'
+import { AgentContext, injectable } from '@aries-framework/core'
 
 import { SdJwtService } from './services'
+
+export type CreateSdJwtOptions = {
+  headerClaims?: Record<string, unknown>
+  nonSelectivelyDisclosableClaims?: Record<string, unknown>
+  selectivelyDisclosableClaims?: Record<string, unknown>
+  defaultDecoyCount?: number
+  // TODO: this could also be a did
+  signerKey: Key
+}
 
 @injectable()
 export class SdJwtApi {
@@ -15,15 +23,7 @@ export class SdJwtApi {
     this.sdJwtService = sdJwtService
   }
 
-  public async createSdJwt(jwt: Record<string, unknown>, claims: Record<string, unknown>, signerKey: Key) {
-    return this.sdJwtService.createSdJwt(this.agentContext, jwt, claims, signerKey)
-  }
-
-  public async discloseClaims(sdJwt: string, claims: string[]): Promise<string> {
-    return this.sdJwtService.discloseClaims(sdJwt, claims)
-  }
-
-  public async verifySdJwt(sdJwt: string): Promise<VERIFYSDJWT_RETURN> {
-    return this.sdJwtService.verifySdJwt(this.agentContext, sdJwt)
+  public async createSdJwt(options: CreateSdJwtOptions) {
+    return this.sdJwtService.createSdJwt(this.agentContext, options)
   }
 }
--- packages/sd-jwt/src/services/SdJwtService.ts
+++ packages/sd-jwt/src/services/SdJwtService.ts
@@ -1,217 +1,83 @@
+import type { CreateSdJwtOptions } from '../SdJwtApi'
 import type { AgentContext, Key } from '@aries-framework/core'
+import type { DisclosureFrame, Hasher as SdJwtHasher, SaltGenerator, Signer } from 'jwt-sd'
 
-import {
-  getJwkFromJson,
-  getJwkFromKey,
-  AriesFrameworkError,
-  Buffer,
-  Hasher,
-  TypedArrayEncoder,
-  EventEmitter,
-  inject,
-  injectable,
-  InjectionSymbols,
-  Logger,
-  JwsService,
-} from '@aries-framework/core'
+import { injectable, getJwkFromKey, TypedArrayEncoder, Hasher } from '@aries-framework/core'
 import { randomBytes } from '@stablelib/random'
-
-interface SD_JWT {
-  _sd: string[]
-  _sd_alg: string
-}
-
-export enum DisclosureArray {
-  SALT = 0,
-  NAME = 1,
-  VALUE = 2,
-}
-
-const SALT_BYTE_SIZE = 128 / 8 // 128-bit salts
-
-export interface VERIFYSDJWT_RETURN {
-  jwt: string
-  disclosed: string
-}
+import { SdJwt } from 'jwt-sd'
 
 @injectable()
 export class SdJwtService {
-  private eventEmitter: EventEmitter
-  private logger: Logger
-
-  private jwsService: JwsService
-
-  public constructor(
-    eventEmitter: EventEmitter,
-    @inject(InjectionSymbols.Logger) logger: Logger,
-    jwsService: JwsService
-  ) {
-    // this.questionAnswerRepository = questionAnswerRepository
-    this.eventEmitter = eventEmitter
-    this.logger = logger
-    this.jwsService = jwsService
+  // TODO: spec says we have to sign the string, not the bytes. Will this work?
+  private hasher(): SdJwtHasher {
+    return (input: string) =>
+      TypedArrayEncoder.toBase64URL(Hasher.hash(TypedArrayEncoder.fromString(input), 'sha2-256'))
   }
 
-  private createDisclosures(claims: Record<string, unknown>) {
-    const disclosures: string[] = []
-    const sdDigests: string[] = []
+  /**
+   * Create a salt with a length of 128 bits
+   */
+  private saltGenerator(): SaltGenerator {
+    return () => TypedArrayEncoder.toBase64URL(randomBytes(128 / 8))
+  }
 
-    const names = Object.keys(claims)
-    const values: unknown[] = Object.values(claims)
-    const salts: Buffer[] = names.map((v) => new Buffer(randomBytes(SALT_BYTE_SIZE)))
-
-    for (let i = 0; i < names.length; i++) {
-      if (typeof values[i] === 'object') {
-        throw new AriesFrameworkError('Nested digests are not supported yet')
-      }
-
-      const disclosureArray = [TypedArrayEncoder.toBase64URL(salts[i]), names[i], values[i]]
-
-      const disclosuresArrayBuffer = TypedArrayEncoder.fromString(JSON.stringify(disclosureArray))
-
-      const disclosure = TypedArrayEncoder.toBase64URL(disclosuresArrayBuffer)
-      disclosures.push(disclosure)
-
-      const disclosureDigest = Hasher.hash(TypedArrayEncoder.fromString(disclosure), 'sha2-256')
-      sdDigests.push(disclosureDigest.toString())
+  // TODO: match the signerKey.keyType to the header.alg
+  private signer(agentContext: AgentContext, signerKey: Key): Signer {
+    return async (input: string, header: Record<string, unknown>) => {
+      return await agentContext.wallet.sign({ key: signerKey, data: TypedArrayEncoder.fromString(input) })
     }
-    return { disclosures, sdDigests: sdDigests.sort() }
   }
 
   public async createSdJwt(
     agentContext: AgentContext,
-    jwt: Record<string, unknown>,
-    claims: Record<string, unknown>,
-    signerKey: Key
+    {
+      headerClaims,
+      signerKey,
+      selectivelyDisclosableClaims,
+      nonSelectivelyDisclosableClaims,
+      defaultDecoyCount,
+    }: CreateSdJwtOptions
   ): Promise<string> {
-    const { disclosures, sdDigests } = this.createDisclosures(claims)
-
-    const sdJwt = {
-      ...jwt,
-      _sd: sdDigests,
-      _sd_alg: 'sha2-256',
-    }
-
-    const sdJwtString = JSON.stringify(sdJwt)
-
-    const payload = TypedArrayEncoder.fromString(sdJwtString)
     const jwk = getJwkFromKey(signerKey)
-    let jws = await this.jwsService.createJwsCompact(agentContext, {
-      key: signerKey,
-      payload: payload,
-      protectedHeaderOptions: {
-        alg: 'ES256',
-        jwk,
-      },
-    })
-
-    if (disclosures) {
-      jws = jws.concat('~' + disclosures.join('~'))
+    const header = {
+      jwk,
+      alg: 'ES256',
+      ...headerClaims,
+    }
+    const payload = {
+      ...selectivelyDisclosableClaims,
+      ...nonSelectivelyDisclosableClaims,
     }
 
-    return jws
+    const disclosureFrame = this.makeDisclosureFrame(selectivelyDisclosableClaims ?? {})
+
+    new SdJwt()
+      .withHeader(header)
+      .withPayload(payload)
+      .withDisclosureFrame(disclosureFrame)
+      .withHasher({ hasher: this.hasher(), algorithm: 'sha-256' })
+      .withSigner(this.signer(agentContext, signerKey))
+      .withSaltGenerator(this.saltGenerator())
+      .toCompact()
+
+    return ''
   }
 
-  public async verifySdJwt(agentContext: AgentContext, sdJwt: string): Promise<VERIFYSDJWT_RETURN> {
-    try {
-      // split SD-JWS into JWS and Disclosures
-      const parts = sdJwt.split('~')
-      const JWS = parts[0]
+  private makeDisclosureFrame(selectivelyDisclosableItems: Record<string, unknown>) {
+    const frame: DisclosureFrame<typeof selectivelyDisclosableItems> = {}
 
-      const [header, _payload] = JWS.split('.')
-
-      const deserializedHeader = JSON.parse(TypedArrayEncoder.fromBase64(header).toString())
-      const stringPayload = TypedArrayEncoder.fromBase64(_payload).toString()
-      const deserializedPayload = JSON.parse(stringPayload)
-
-      // // verify the JWS
-      let payload = ''
-      try {
-        // @ts-ignore
-        const issuerPublicJwk = getJwkFromJson(deserializedHeader.jwk)
-
-        const signatureResult = await this.jwsService.verifyJws(agentContext, {
-          jws: JWS,
-          jwkResolver: () => issuerPublicJwk,
-        })
-
-        if (!signatureResult.isValid) {
-          throw new Error('Signature validation failed')
+    const createFrame = (data: Record<string, unknown>) => {
+      Object.entries(data).forEach(([key, value]) => {
+        if (typeof value === 'object' && !Array.isArray(value) && value !== null) {
+          createFrame(value as Record<string, unknown>)
+        } else {
+          frame[key] = true
         }
-
-        // const result = await jose.compactVerify(JWS, jose.createLocalJWKSet(jwks))
-        payload = stringPayload
-      } catch (err) {
-        throw new Error(`Error validating signature: ${err}`)
-      }
-
-      // verify the Disclosures, if any
-      // TODO: generalized for nested objects
-      let disclosedClaims = {}
-      if (parts.length > 1) {
-        const disclosures = parts.slice(1)
-
-        const sd = (deserializedPayload as SD_JWT)._sd
-        const sd_alg = (deserializedPayload as SD_JWT)._sd_alg
-        disclosedClaims = this.verifyDisclosures(disclosures, sd_alg, sd)
-      }
-
-      const disclosedClaimsString = JSON.stringify(disclosedClaims)
-
-      return {
-        jwt: payload,
-        disclosed: disclosedClaimsString,
-      }
-    } catch (err) {
-      throw new Error(`Can't verify the SD-JWT: ${err as string}`)
-    }
-  }
-
-  private verifyDisclosures(disclosures: string[], sd_alg: string, sd: string[]) {
-    const disclosedClaims = {}
-    disclosures.forEach((disclosure) => {
-      const disclosureDigest = this.hashDisclosure(sd_alg, disclosure)
-
-      if (!sd.includes(disclosureDigest)) {
-        throw new Error(`Disclosure ${disclosure} is not contained in SD-JWT`)
-      }
-      const disclosureArray = this.parseDisclosure(disclosure)
-      Object.defineProperty(disclosedClaims, disclosureArray[DisclosureArray.NAME], {
-        value: disclosureArray[DisclosureArray.VALUE],
-        enumerable: true,
       })
-    })
-
-    return disclosedClaims
-  }
-
-  public async discloseClaims(sdJwt: string, claims: string[]): Promise<string> {
-    // split SD-JWS into JWS and Disclosures
-    const parts = sdJwt.split('~')
-    if (parts.length <= 1) {
-      throw new Error('No Disclosures found in SD-JWT')
     }
-    const JWS = parts[0]
-    let disclosures = parts.slice(1)
-    disclosures = disclosures.filter((disclosure) =>
-      claims.includes(this.parseDisclosure(disclosure)[DisclosureArray.NAME])
-    )
 
-    // re-encode the updated SD-JWT w/ Disclosures
-    const updatedSdJwt = JWS.concat('~' + disclosures.join('~'))
-    return updatedSdJwt
-  }
+    createFrame(selectivelyDisclosableItems)
 
-  private hashDisclosure(alg: string, disclosure: string): string {
-    return Hasher.hash(TypedArrayEncoder.fromString(disclosure), 'sha2-256').toString()
-  }
-
-  private parseDisclosure = (disclosure: string): string[] => {
-    const input = TypedArrayEncoder.fromBase64(disclosure)
-    const parsed: string[] = JSON.parse(input.toString())
-    if (parsed.length != 3) {
-      throw new Error("can't parse disclosure: " + disclosure)
-    }
-    return parsed
+    return frame
   }
 }
